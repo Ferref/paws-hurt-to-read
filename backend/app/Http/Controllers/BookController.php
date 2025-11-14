86772181f3da6566dc5c1d61ad982165bea8da9a
Ferenc Kovacs <kovacsferenc026@gmail.com>
2025-11-14 15:21:16 +0100

    Get books through api call

---
+++ backend/app/Http/Controllers/BookController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Repositories\BookRepository;
+
+class BookController extends Controller
+{
+
+    public function __construct() {
+        $this->bookRepository = app(BookRepository::class);
+    }
+    
+    public function getBook(int $id): JsonResponse
+    {
+        $book = $this->bookRepository->getBookById($id);
+        return response()->json($book);
+    }
+
+    public function getBookCovers(string $range): JsonResponse
+    {
+        $ids = explode('-', $range);
+        $min = (int) $ids[0];
+        $max = (int) $ids[1];
+
+        $books = $this->bookRepository->getBookCovers($min, $max);
+        return response()->json($books);
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/book/{id}', [BookController::class, 'getBook']);
+Route::get('/book-covers/{range}', [BookController::class, 'getBookCovers']);
