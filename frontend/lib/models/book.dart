64567ba1a422b99388eec2525a149379fc4ef7f3
Ferenc Kovacs <kovacsferenc026@gmail.com>
2025-11-15 11:54:51 +0100

    Simplified json conversion

--- frontend/lib/models/book.dart
+++ frontend/lib/models/book.dart
@@ -1,25 +1,14 @@
 class Book {
-  final int? id;
+  final int id;
   final String title;
-  final String coverImage;
+  final String? coverImage;
 
-  Book({this.id, required this.title, required this.coverImage});
+  Book({required this.id, required this.title, this.coverImage});
 
   factory Book.fromJson(Map<String, dynamic> json) {
-    int? parseInt(dynamic value) {
-      if (value == null) {
-        return null;
-      }
-
-      if (value is int) {
-        return value;
-      }
-      return int.tryParse(value.toString());
-    }
-
-    final int? id = parseInt(json['id']);
-    final String title = (json['title']?.toString() ?? 'Untitled').trim();
-    final String coverImage = json['cover_image']?.toString() ?? '';
+    final int id = int.parse(json['id'].toString());
+    final String title = json['title']!.toString().trim();
+    final String? coverImage = json['cover_image']?.toString();
 
     return Book(id: id, title: title, coverImage: coverImage);
   }
