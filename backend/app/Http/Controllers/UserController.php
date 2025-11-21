ddf2ad73f917d4fe54b1e0b84c87e51f25aad5df
Ferenc Kovacs <kovacsferenc026@gmail.com>
2025-11-21 12:41:31 +0100

    Change route names (organized routes)

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -9,35 +9,5 @@ use App\Models\Book;
 
 class UserController extends Controller
 {
-    public function addToMyBooks(Request $request): JsonResponse
-    {
-        $validated = $request->validate([
-            'id' => 'required|integer|exists:books,id'
-        ]);
-
-        $bookId = $validated['id'];
-
-        $user = auth()->user();
-
-        if (!$user) {
-            return response()->json(['message' => 'Unauthenticated'], 401);
-        }
-
-        $user->books()->syncWithoutDetaching([$bookId]);
-
-        return response()->json(['message' => 'Book added succesfully'], 200);
-    }
-
-    public function getMyBooks(): JsonResponse
-    {
-        $user = auth()->user();
-
-        if (!$user) {
-            return response()->json(['message' => 'Unauthenticated'], 401);
-        }
-
-        $books = $user->books();
-
-        return response()->json(json_encode($books), 200);
-    }
+    // Implement
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -8,8 +8,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/book-details/{id}', [BookController::class, 'getBookDetailsById']);
-Route::get('/book-covers/{range}', [BookController::class, 'getBookCoversByIdRange']);
-
-Route::get('user-books', [UserController::class, 'getMyBooks']);
-Route::post('user-books/{id}', [UserController::class, 'addToMyBooks']);
+Route::get('/book/details/{id}', [BookController::class, 'getBookDetailsById']);
+Route::get('/book/covers/{range}', [BookController::class, 'getBookCoversByIdRange']);
